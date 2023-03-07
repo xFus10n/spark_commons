2c1c3e024fbac9cfcabce7ade068fb35754b72c5
rrazumilovs <razumilovrenat@inbox.lv>
2023-03-07 11:29:15 +0200

    test melt

---
+++ df_utils/operation.py
@@ -0,0 +1,49 @@
+from pyspark.sql import DataFrame, functions as F
+from pyspark.sql.types import StringType, DataType
+from functools import partial, reduce
+
+
+def melt(df, keep_cols, melt_cols, var_name, value_name):
+    _vars_and_vals = F.array(*(F.struct(F.lit(c).alias(var_name), F.col(c).alias(value_name)) for c in melt_cols))
+    _tmp = df.withColumn("_vars_and_vals", F.explode(_vars_and_vals))
+    cols = keep_cols + [F.col("_vars_and_vals")[x].alias(x) for x in [var_name, value_name]]
+
+    return _tmp.select(*cols)
+
+
+def stack(df: DataFrame, columns: list, var_col: str, var_val: str) -> DataFrame:
+    stack_cols: str = ''.join(f"'{c}',{c}," for c in columns)[:-1]  # without last comma
+    exp = "stack(" + str(len(columns)) + "," + stack_cols + f") as ({var_col}, {var_val})"
+    return df.selectExpr("*", exp).drop(*columns)
+
+
+def union_all(*dfs, allow_missing_columns: bool = False):
+    return reduce(
+        partial(DataFrame.unionByName, allowMisingColumns=allow_missing_columns), dfs
+    )
+
+
+def _rename_dict(df: DataFrame, rename_dict: dict):
+    _new_cols = [rename_dict.get(i, i) for i in df.columns]
+    return df.toDF(*_new_cols)
+
+
+def add_missing_columns(df: DataFrame, ref_df: DataFrame) -> DataFrame:
+    """Add missing columns from ref_df to df
+
+    Args:
+        df (DataFrame): dataframe with missing columns
+        ref_df (DataFrame): referential dataframe
+
+    Returns:
+        DataFrame: df with additionnal columns from ref_df
+    """
+    for col in ref_df.schema:
+        if col.name not in df.columns:
+            df = df.withColumn(col.name, F.lit(None).cast(col.dataType))
+
+    return df
+
+
+def cast_columns(df: DataFrame, colz: list, new_type: DataType = StringType()) -> DataFrame:
+    return df.select([F.col(c).cast(new_type) if c in colz else c for c in df.columns])
--- tests/operation_tests/operation_test.py
+++ tests/operation_tests/operation_test.py
@@ -1,8 +1,9 @@
 import logging
 import pytest
 from pyspark_test import assert_pyspark_df_equal
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as F
 from tests.utilz.df_test_helper import compare, create_df
+from df_utils.operation import melt
 
 
 def quiet_py4j():
@@ -20,19 +21,34 @@ def spark_session(request):
 
 def test_top_floor_extract(spark_session):
     # arrange
-    test_df = get_output_for_top_floor(spark_session)
-    expected_df = get_output_for_top_floor(spark_session)
+    test_df = get_test_data(spark_session)
+    expected_df = get_expect_data(spark_session)
 
     # act
-    # output_df = main.set_top_floor(test_df)
-    # actual_df = output_df.select(main.floor, main.top_floor) if hasattr(output_df, main.top_floor) else None
+    output_df = melt(test_df, ["col1", "col2", "col3"], ["Y1", "Y2", "Y3"], "category", "value").sort(F.col("value"))
 
     # assert
-    compare(expected_df, test_df)
-    assert_pyspark_df_equal(expected_df, test_df)
+    compare(expected_df, output_df)
+    assert_pyspark_df_equal(expected_df, output_df)
 
 
-def get_output_for_top_floor(spark_session):
-    columns = 'top_floor', 'count'
-    data = [("5", 3), ("3", 2), ("2", 1), ("10", 1)]
+def get_test_data(spark_session):
+    columns = ("col1", "col2", "col3", "Y1", "Y2", "Y3")
+    data = [(4000521, "BOF_I2189490CA-20", "89EB", 1000.01, 2000.02, 3000.03),
+            (4000521, "BOF_I2189490CA-20", "89EK", 4000.04, 5000.05, 6000.06),
+            (4000521, "BOF_I2189490CA-20", "89EN", 7000.07, 8000.08, 9000.09)]
+    return create_df(columns, data, spark_session)
+
+
+def get_expect_data(spark_session):
+    columns = ("col1", "col2", "col3", "category", "value")
+    data = [(4000521, "BOF_I2189490CA-20", "89EB", "Y1", 1000.01),
+            (4000521, "BOF_I2189490CA-20", "89EB", "Y2", 2000.02),
+            (4000521, "BOF_I2189490CA-20", "89EB", "Y3", 3000.03),
+            (4000521, "BOF_I2189490CA-20", "89EK", "Y1", 4000.04),
+            (4000521, "BOF_I2189490CA-20", "89EK", "Y2", 5000.05),
+            (4000521, "BOF_I2189490CA-20", "89EK", "Y3", 6000.06),
+            (4000521, "BOF_I2189490CA-20", "89EN", "Y1", 7000.07),
+            (4000521, "BOF_I2189490CA-20", "89EN", "Y2", 8000.08),
+            (4000521, "BOF_I2189490CA-20", "89EN", "Y3", 9000.09)]
     return create_df(columns, data, spark_session)
